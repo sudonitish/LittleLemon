776d6db1db3c48a1a26d6da294485e2ea9f4a5c8
sudonitish <90000nitish@gmail.com>
2025-07-22 21:51:05 +0530

    models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+
+class MenuTable(models.Model):
+    id = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
