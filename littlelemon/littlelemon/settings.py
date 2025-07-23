775ed2aecf43655d2c800ef5ceddf77327a0c997
sudonitish <90000nitish@gmail.com>
2025-07-23 08:39:19 +0530

    dsojer

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "restaurant",
     "rest_framework",
+    "djoser",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -123,7 +125,19 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Djoser settings
+DJOSER = {"USER_ID_FIELD": "username"}
+
+# Django REST Framework authentication settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,15 +3,17 @@ from rest_framework import generics
 from .models import MenuTable, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import viewsets
-
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, "index.html", {})
 
-
+@permission_classes([IsAuthenticated])
 class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuTable.objects.all()
-    serializer_class = MenuSerializer
+   permission_classes = [IsAuthenticated]
+   queryset = MenuTable.objects.all()
+   serializer_class = MenuSerializer
 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
