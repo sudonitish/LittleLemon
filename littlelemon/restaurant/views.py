6307cf155e128f42f3f87b14d73a70cb3095f78a
sudonitish <90000nitish@gmail.com>
2025-07-23 21:12:41 +0530

    readme

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,23 +3,25 @@ from rest_framework import generics
 from .models import MenuTable, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import viewsets
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, "index.html", {})
 
-@permission_classes([IsAuthenticated])
+
 class MenuItemsView(generics.ListCreateAPIView):
-   permission_classes = [IsAuthenticated]
-   queryset = MenuTable.objects.all()
-   serializer_class = MenuSerializer
+    queryset = MenuTable.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuTable.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
